e8a8e958e0653ef0da709c8e4094cf20c8661b1d
nathan293323 <nathan293323@gmail.com>
2024-07-08 17:27:47 +0800

    建立一個nathan的練習網站

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# 註解筆記
-# 有註解的行
-"""
-使用多行註解
-"""
-# print("Hello World!") # ctrL+? 可以單行註解
-print("123")  # 可以在code後面加上'#'來當作註解
+import streamlit as st
 
-# ctrl+s代表存檔
-# ctrL+右或左可以部份註解
+st.title("nathan的練習網站")
