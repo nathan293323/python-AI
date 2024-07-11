6b738e9c10b5eb9dad5fc481de8ef081fea74e6a
nathan293323 <nathan293323@gmail.com>
2024-07-11 16:29:14 +0800

    class4-5

--- pages/class4-5.py
+++ pages/class4-5.py
@@ -6,9 +6,14 @@ image_folder = "image"  # 資料夾名稱
 image_files = os.listdir(image_folder)  # 取得資料夾裡面的所有檔案
 st.write(image_files)  # 顯示所有檔案
 
-image_size = st.number_input("欄數", min_value=1, max_value=3, step=1, value=1)
-# 使用者輸入圖片大小, 最小50 , 最大500, , 每次增加50, 預設100
-
+image_size = st.number_input("圖片大小", min_value=1, step=1, value=1)
 for image_file in image_files:  # 顯示所有圖片
+    st.image(
+        f"{image_folder}/{image_file}",
+        width=image_size,
+    )
+    # 顯示圖片, 根據使用者輸入的大小調整寬度
+
+for image_file in image_files:  #  顯示所有圖片
     st.image(f"{image_folder}/{image_file}", use_column_width=True)
     # 顯示圖片, 使用欄寬度
