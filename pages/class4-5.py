202e3dac31454160fa892bb6e80eccca28ad4e97
nathan293323 <nathan293323@gmail.com>
2024-07-11 16:30:27 +0800

    class4-5

--- pages/class4-5.py
+++ pages/class4-5.py
@@ -4,7 +4,6 @@ import os
 st.title("購物平台")
 image_folder = "image"  # 資料夾名稱
 image_files = os.listdir(image_folder)  # 取得資料夾裡面的所有檔案
-st.write(image_files)  # 顯示所有檔案
 
 image_size = st.number_input("圖片大小", min_value=1, step=1, value=1)
 for image_file in image_files:  # 顯示所有圖片
